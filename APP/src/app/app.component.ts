5778f79d1f2c0e3c30c40f294cc21ad68922367e
Stephen Rifenburgh <Stephen@Rifenburgh.com>
2017-03-26 17:28:23 -0400

    Style sheets

--- APP/src/app/app.component.ts
+++ APP/src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { SessionService } from './services/session.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'W9D5E2';
+  person: Object = {};
 
+  constructor(
+    private mySession: SessionService
+  ) {}
+  ngOnInit() {
+    this.mySession.isLoggedIn()
+      .subscribe((user) => {
+        this.person = user.json();
+      })
+  }
 }
